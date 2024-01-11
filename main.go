7c695f18e36d45bb40600a8dc268df8bc551c0c5
vngoldstreet <vngoldstreet@gmail.com>
2024-01-11 11:12:15 +0700

    admin

--- main.go
+++ main.go
@@ -1,80 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-// type RequestInfo struct {
-// 	IP  string
-// 	UID uint
-// }
+var (
+	onceMap    sync.Map
+	isApproved bool
+)
 
-// type ClientTracker struct {
-// 	Counters   map[RequestInfo]int
-// 	Mutex      sync.Mutex
-// 	LockTimers map[RequestInfo]*time.Timer
-// }
+func ApprovalMiddleware(fullPath string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Printf("c.FullPath(): %v\n", c.FullPath())
+		if c.Request.Method == http.MethodPost && c.FullPath() == fullPath {
+			// Sử dụng sync.Once để đảm bảo middleware chỉ được thực hiện một lần
+			fmt.Printf("fullPath: %v\n", fullPath)
+			onceKey := fullPath
+			_, exists := onceMap.LoadOrStore(onceKey, &sync.Once{})
 
-// func NewClientTracker() *ClientTracker {
-// 	return &ClientTracker{
-// 		Counters:   make(map[RequestInfo]int),
-// 		Mutex:      sync.Mutex{},
-// 		LockTimers: make(map[RequestInfo]*time.Timer),
-// 	}
-// }
+			if !exists {
+				// Thực hiện logic xử lý yêu cầu ở đây
+				fmt.Printf("%s logic executed.\n", fullPath)
+				isApproved = true
+			}
 
-// func TokenAndIPFilterMiddleware(tracker *ClientTracker) gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 		if c.Request.Method == http.MethodPost {
-// 			ip := c.ClientIP()
-// 			uuid, err := ExtractTokenID(c)
-// 			if err != nil {
-// 				return
-// 			}
-
-// 			requestInfo := RequestInfo{
-// 				IP:  ip,
-// 				UID: uuid,
-// 			}
-
-// 			tracker.Mutex.Lock()
-// 			defer tracker.Mutex.Unlock()
-
-// 			tracker.Counters[requestInfo]++
-// 			if tracker.Counters[requestInfo] > 10 {
-// 				if _, ok := tracker.LockTimers[requestInfo]; !ok {
-// 					fmt.Println("Client locked. UID:", requestInfo.UID)
-
-// 					// tx := db_ksc.Begin()
-// 					// resUser := CpsUsers{}
-// 					// if err := tx.Where("id = ?", uuid).First(&resUser).Error; err != nil {
-// 					// 	tx.Rollback()
-// 					// 	return
-// 					// }
-
-// 					// msg := fmt.Sprintf("Too many requests from this client: %d - %s (%s). Locked!", resUser.ID, resUser.Name, resUser.Email)
-// 					// SendMessageToAccountGroup(msg)
-
-// 					// tx.Commit()
-
-// 					tracker.LockTimers[requestInfo] = time.AfterFunc(3*time.Second, func() {
-// 						fmt.Println("Client unlocked. UID:", requestInfo.UID)
-// 						delete(tracker.LockTimers, requestInfo)
-// 						tracker.Counters[requestInfo] = 0
-// 					})
-// 				}
-
-// 				c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{"error": "Too many requests from this client"})
-// 				return
-// 			}
-// 		}
-// 		c.Next()
-// 	}
-// }
+			if !isApproved {
+				// Nếu không được phê duyệt, trả về lỗi
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Not approved"})
+				return
+			}
+		}
+		c.Next()
+	}
+}
 
 func main() {
 	r := gin.Default()
@@ -157,19 +121,22 @@ func main() {
 
 	private := r.Group("/auth")
 	private.Use(JwtAuthMiddleware())
-	// private.Use(TokenAndIPFilterMiddleware(clientTracker))
 	private.POST("/upload-csv", upLoadFunc)
 	private.POST("/upload-old-leaderboard", upLoadOldLeaderboard)
 	private.POST("/create-contest", createContest)
 	private.POST("/create-transaction", createTransactions)
 	private.POST("/update-contest-id", updateContestByID) //done
 
+	private.Use(ApprovalMiddleware("/contest-approval"))
 	private.POST("/contest-approval", approvalContest) //done
 	private.GET("/contest/get-account-store", contestGetAccountStore)
 	private.GET("/contest/get-current-contest", contestGetCurrentContest)
 	private.POST("/contest/send-email", sendEmailFromAdmin)
 
-	private.POST("/rejoin-contest-approval", approvalRejoinContest)                    //done
+	private.Use(ApprovalMiddleware("/rejoin-contest-approval"))
+	private.POST("/rejoin-contest-approval", approvalRejoinContest) //done
+
+	private.Use(ApprovalMiddleware("/admin-transaction"))
 	private.POST("/admin-transaction", approvalTransactions)                           //done
 	private.POST("/cancel-transaction", cancelTransactions)                            //done
 	private.GET("/get-transaction-list", getTransactions)                              //done
